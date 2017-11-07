fbc51ce61ccec6dd4f37217b8875e37d9f0cd221
vbaghdas <vbaghdas@gmail.com>
2017-11-07 01:25:34 -0800

    adjusted time on modal timeout

--- js/game.js
+++ js/game.js
@@ -141,7 +141,7 @@ function Game() {
 
                     if(this.matchCount === this.cardList.length){
                         this.soundList.victory.play();
-                        setTimeout( ()=>{ this.victoryModal() }, 500);
+                        setTimeout( ()=>{ this.victoryModal() }, 1000);
                         setTimeout( ()=>{ this.levelChange() }, this.revertTime);
                     }
                 }
